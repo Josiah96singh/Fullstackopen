ef4d4036410715d282e4b38d0e94d6af80ba9437
josiahsingh <josiahsingh1996@gmail.com>
2024-11-11 00:38:38 +0200

    started course information app 1.1 & 1.2

---
+++ demoapps/part1/part1/src/App.jsx
@@ -0,0 +1,20 @@
+
+// const Hello = (props) => {
+//   return (
+//     <>
+//     <p>Hello, my name is {props.name} and my favourite animal is {props.animal}</p>
+//     </>
+//   )
+// }
+
+const App = () => {
+  const friends = ['monkey', 'donkey']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
